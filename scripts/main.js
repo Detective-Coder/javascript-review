6b78d86ac5d392caf9ca59668c297663690b4404
Detective-Coder <aaronfrankenfield@gmail.com>
2021-01-11 10:06:24 -0500

    first lightning exercise, variable, object, array of objects

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+console.log("Welcome to the main module")
+const favoriteSandwich = "Subway Club";
+var leastFaveSandwich = {
+  name: "Egg Salad",
+  price: 12,
+  ingredients: ["egg", "salad", "mayo"]
+};
+console.log(leastFaveSandwich);
+
+let restaurantMenu = [
+  {
+    "name": "Subway Club",
+    "price": 5.99,
+    "ingredients": ["meats", "cheese", "veggies"]
+  },
+  {
+    "name": "Egg Salad",
+    "price": 12,
+    "ingredients": ["egg", "salad", "mayo"]
+  },
+  {
+    "name": "Meatball Sub",
+    "price": 8.50,
+    "ingredients": ["meatballs", "cheese", "peppers"]
+  }
+]
+console.log(restaurantMenu);
